7075d16d128416f97e4a7e6c388b1aca23bf5805
SaganVV <39135216+SaganVV@users.noreply.github.com>
2021-08-19 19:26:20 +0300

    Update 334. Increasing Triplet Subsequence.py

--- Medium/334. Increasing Triplet Subsequence.py
+++ Medium/334. Increasing Triplet Subsequence.py
@@ -10,3 +10,6 @@ class Solution:
             elif n>min2:
                 return True
         return False
+"""Firstly, we find 2 minimum current value. If the next number less then first, we can put this number at min1 because answer cant change fater this operation
+(if increasing triplet exist like num1<num2<num3 then if we have num4 < num1, then num4<num2<num3; If num1<num2 and num4<num1 and we have found num3: num4<num2<num3
+=> num1<num2<num3 and such triplet exist)"""
