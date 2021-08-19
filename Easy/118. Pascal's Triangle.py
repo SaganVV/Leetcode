7d5e2d644d5d9f89f7a6f048ad7ee35ed186557d
SaganVV <39135216+SaganVV@users.noreply.github.com>
2021-08-19 19:18:20 +0300

    Create 118. Pascal's Triangle.py

---
+++ Easy/118. Pascal's Triangle.py
@@ -0,0 +1,10 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        triangle = [[1]]
+        
+        for i in range(1, numRows):
+            triangle.append([1])
+            for j in range(i-1):
+                triangle[-1].append(triangle[-2][j]+triangle[-2][j+1])
+            triangle[-1].append(1)
+        return triangle
