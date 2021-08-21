cd0d8494db2dfb39ac9b82d3bf88480074681ee3
SaganVV <39135216+SaganVV@users.noreply.github.com>
2021-08-21 21:03:40 +0300

    Create 448. Find All Numbers Disappeared in an Array.py
    Just set difference between all numbers from 1 to n and nums

---
+++ Easy/448. Find All Numbers Disappeared in an Array.py
@@ -0,0 +1,5 @@
+# Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        return list(set(range(1, len(nums)+1))-set(nums))
