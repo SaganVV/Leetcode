0d0c0cc12a16c3c6ae68bef0e564bbdb62d7e2ae
SaganVV <39135216+SaganVV@users.noreply.github.com>
2021-08-22 20:35:58 +0300

    Create 198. House Robber.py

---
+++ Medium/198. House Robber.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        max_money = [0, nums[0]]
+        numb_last_house = 0
+        for i in range(1, len(nums)):
+            if i-1 == numb_last_house:
+                max_with_cur_house = max_money[-2]+nums[i]
+                if max_with_cur_house > max_money[-1]:
+                    max_money.append(max_with_cur_house)
+                    numb_last_house = i
+                else:              
+                    max_money.append(max_money[-1])
+            else:
+                
+                max_money.append(max_money[-1]+nums[i])
+                numb_last_house = i
+           # print(i, max_money, nums)  
+        return max_money[-1]
+# Used dynamic programming approach. At the "numb_last_house" i keep number of the last house, what was robed at best solution at processing dynumic programming
+# max_money[i] - its max money, that we can rob from first "i" house
+# Other we can understand from code
